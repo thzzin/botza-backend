bdb318a125d1edaca7f4909780f2ba48f8b6e201
thzzin <thithipaiva@hotmail.com>
2024-11-21 02:39:35 -0300

    ssl

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,49 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as bodyParser from 'body-parser';
+import * as https from 'https';
+import * as fs from 'fs';
+
+const privateKey = fs.readFileSync(
+  "/etc/letsencrypt/live/getluvia.com.br/privkey.pem",
+  "utf8"
+);
+const certificate = fs.readFileSync(
+  "/etc/letsencrypt/live/getluvia.com.br/cert.pem",
+  "utf8"
+);
+const ca = fs.readFileSync(
+  "/etc/letsencrypt/live/getluvia.com.br/chain.pem",
+  "utf8"
+);
+
+const httpsOptions = {
+  key: privateKey,
+  cert: certificate,
+  ca: ca,
+};
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Habilita o CORS (caso queira permitir acesso de outros domínios)
+  // Habilita CORS (opcional)
   app.enableCors({
-    origin: '*', // ou restrinja para um domínio específico
+    origin: '*', // Altere para um domínio específico em produção
     methods: 'GET,POST,PUT,DELETE',
     allowedHeaders: 'Content-Type,Authorization',
   });
 
-  // Configura o body parser para aceitar requisições maiores
+  // Configura body-parser
   app.use(bodyParser.json({ limit: '50mb' }));
   app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
-  // Inicia o servidor na porta configurada (padrão 3005)
-  await app.listen(process.env.PORT ?? 3010);
+  // Cria o servidor HTTPS
+  const server = https.createServer(httpsOptions, app.getHttpAdapter().getInstance());
+  const PORT = process.env.PORT || 3010;
+  server.listen(PORT, () => {
+    console.log(`HTTPS Server running on port ${PORT}`);
+  });
 }
 bootstrap();
+
