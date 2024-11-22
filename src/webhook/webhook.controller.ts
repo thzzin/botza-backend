7d3939e67a80e97e6f00743760775cad9f56b065
thzzin <thithipaiva@hotmail.com>
2024-11-21 21:07:19 -0300

    webhook token

--- src/webhook/webhook.controller.ts
+++ src/webhook/webhook.controller.ts
@@ -13,7 +13,7 @@ export class WebhookController {
     private readonly conversationService: ConversationService,
     private readonly messageService: MessageService,
     private readonly adminService: AdminService,
-    private readonly verifyToken = 'EAAMaEvMlYFYBOzM0Ga4ef907rL4NXA6IwM8z5uc7ZAs3lJZBfd2wrxk9j3F5ldTE7duE7eZCpRCyGiFWYTPsqz9W7az2gAyujlhyKrMd5ocyWAxWm6sIZAlv26YWHSmlbycmbZAg86tYvDjkZAPIUdteBBWhHmzL6jyeQCKhddcw9xJIqgp0SlmxnYwESponYZCy0nxKuPlCnczOKZAwPTBquV3hYE0ZD';
+    private readonly verifyToken = 'EAAMaEvMlYFYBOzM0Ga4ef907rL4NXA6IwM8z5uc7ZAs3lJZBfd2wrxk9j3F5ldTE7duE7eZCpRCyGiFWYTPsqz9W7az2gAyujlhyKrMd5ocyWAxWm6sIZAlv26YWHSmlbycmbZAg86tYvDjkZAPIUdteBBWhHmzL6jyeQCKhddcw9xJIqgp0SlmxnYwESponYZCy0nxKuPlCnczOKZAwPTBquV3hYE0ZD'
   ) {}
 
    @Get()
